4f590871d9cb8b2995aa37bac448e44b49bf8062
Anthuan Vásquez <eigthy@gmail.com>
2014-07-29 16:18:58 -0400

    Agregar sidebar para woocommerce

---
+++ sidebar-product.php
@@ -0,0 +1,5 @@
+<div class="widget-area" role="complementary">
+
+	<?php if ( dynamic_sidebar( 'product-sidebar' ) ) : endif; ?>
+
+</div><!-- .widget-area (end) -->
---
+++ sidebar-shop.php
@@ -0,0 +1,5 @@
+<div class="widget-area" role="complementary">
+
+	<?php if ( dynamic_sidebar( 'shop-sidebar' ) ) : endif; ?>
+
+</div><!-- .widget-area (end) -->
