c091336704ceb3919851d4352fcf20ed821ce451
Anthuan Vásquez <eigthy@gmail.com>
2014-08-28 16:33:53 -0400

    Para mostras los posts en columnas

---
+++ template-post_grid.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ Template Name: Post Grid
+ */
+
+get_header();
+
+$classes = 'blog-2-cols';
+
+?>
+
+
+<div class="grid-columns row-fluid">
+	<div class="full-width">
+		<div class="site-main <?php echo $classes; ?> group" role="main">
+			<?php
+				// First, initialize how many posts to render per page
+				$number = get_option( 'posts_per_page' );
+				
+				// Next, get the current page
+				$page = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+
+				// After that, calculate the offset
+				$offset = ( $page - 1 ) * $number;
+
+				// Finally, we'll set the query arguments and instantiate WP_Query
+				$query_args = array(
+				  'post_type'  =>  array( 'post' ),
+				  'posts_per_page' => $number,
+				  'orderby'    =>  'date',
+				  'order'      =>  'desc',
+				  'number'     =>  $number,
+				  'page'       =>  $page,
+				  'offset'     =>  $offset
+				);
+
+				$the_query = new WP_Query( $query_args );
+					
+				if ( $the_query->have_posts() ) :
+					
+					while ( $the_query->have_posts()) : $the_query->the_post();						
+						get_template_part( 'content', 'post' );
+					endwhile;
+					
+					tm_num_pagination($the_query->max_num_pages);
+					wp_reset_query();
+
+				endif;
+			?>	
+
+		</div><!-- .site-main (end) -->
+	</div><!-- .content-area (end) -->
+	
+</div><!-- .grid-columns (end) -->
+
+<?php get_footer(); ?>
