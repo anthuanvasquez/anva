14921b2dbd897055a4dc7ad20bf2ea50104d2264
Anthuan Vásquez <eigthy@gmail.com>
2014-09-02 18:01:57 -0400

    Se ha agregado un widget para los posts personalizados

--- framework/includes/widgets.php
+++ framework/includes/widgets.php
@@ -3,10 +3,12 @@
 include_once( TM_THEME_FRAMEWORK . '/includes/widgets/class-widget-social-media.php' );
 include_once( TM_THEME_FRAMEWORK . '/includes/widgets/class-widget-custom-services.php' );
 include_once( TM_THEME_FRAMEWORK . '/includes/widgets/class-widget-custom-contact.php' );
+include_once( TM_THEME_FRAMEWORK . '/includes/widgets/class-widget-custom-posts.php' );
 
 /* Register Widgets */
 function tm_register_widgets() {
 	register_widget( 'Custom_Social_Media' );
 	register_widget( 'Custom_Services' );
 	register_widget( 'Custom_Contact' );
+	register_widget( 'Custom_Posts' );
 }
