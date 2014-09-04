eb9d9f42a335cf9ab593f245a9939d1e117d1116
Anthuan Vásquez <eigthy@gmail.com>
2014-09-04 11:14:24 -0400

    Incluido el meta para mostrar los titulos de las paginas por separados

--- content-page.php
+++ content-page.php
@@ -1,6 +1,12 @@
+<?php
+	$hide_title = get_post_meta( $post->ID, '_hide_title', true );
+?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php if ( 'show' == $hide_title ) : ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php endif; ?>
 		<div class="entry-meta">
 			<?php
 				$single_meta = tm_get_option( 'single_meta' );
