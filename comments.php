4cf0798203e19b21b478fe63150deb9eb1ffc2b7
Anthuan Vásquez <eigthy@gmail.com>
2014-09-09 11:32:07 -0400

    Removido comentarios

--- comments.php
+++ comments.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * The template for displaying Comments.
- *
- * The area of the page that contains both current comments
- * and the comment form.
- *
- * @package _s
- */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
 if ( post_password_required() ) {
 	return;
 }
