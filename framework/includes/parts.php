0f4ff9998fc05f4ebc605fd13b43239f0faa730e
Anthuan Vásquez <eigthy@gmail.com>
2014-10-02 17:55:32 -0400

    Acentos

--- framework/includes/parts.php
+++ framework/includes/parts.php
@@ -12,25 +12,25 @@ function tm_archive_title() {
 		printf( __( 'Autor: %s', TM_THEME_DOMAIN ), '<span class="vcard">' . get_the_author() . '</span>' );
 
 	elseif ( is_day() ) :
-		printf( __( 'Dia: %s', TM_THEME_DOMAIN ), '<span>' . get_the_date() . '</span>' );
+		printf( __( 'Día: %s', TM_THEME_DOMAIN ), '<span>' . get_the_date() . '</span>' );
 
 	elseif ( is_month() ) :
 		printf( __( 'Mes: %s', TM_THEME_DOMAIN ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', TM_THEME_DOMAIN ) ) . '</span>' );
 
 	elseif ( is_year() ) :
-		printf( __( 'Ano: %s', TM_THEME_DOMAIN ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', TM_THEME_DOMAIN ) ) . '</span>' );
+		printf( __( 'Año: %s', TM_THEME_DOMAIN ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', TM_THEME_DOMAIN ) ) . '</span>' );
 
 	elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
 		_e( 'Asides', TM_THEME_DOMAIN );
 
 	elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-		_e( 'Galerias', TM_THEME_DOMAIN);
+		_e( 'Galerías', TM_THEME_DOMAIN);
 
 	elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-		_e( 'Imagenes', TM_THEME_DOMAIN);
+		_e( 'Imágenes', TM_THEME_DOMAIN);
 
 	elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-		_e( 'Videos', TM_THEME_DOMAIN );
+		_e( 'Vídeos', TM_THEME_DOMAIN );
 
 	elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
 		_e( 'Citas', TM_THEME_DOMAIN );
