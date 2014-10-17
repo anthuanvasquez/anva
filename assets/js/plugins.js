07390726058e4c44e52bac2f7131a60532524f09
Anthuan Vásquez <eigthy@gmail.com>
2014-10-17 16:22:28 -0400

    Compilaciones.

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -3,20 +3,20 @@
 	var method;
 	var noop = function () {};
 	var methods = [
-			'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-			'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-			'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-			'timeStamp', 'trace', 'warn'
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeStamp', 'trace', 'warn'
 	];
 	var length = methods.length;
-	var console = (window.console = window.console || {});
+	var console = ( window.console = window.console || {} );
 
-	while (length--) {
+	while ( length-- ) {
 		method = methods[length];
 
 		// Only stub undefined methods.
 		if (!console[method]) {
-				console[method] = noop;
+			console[method] = noop;
 		}
 	}
 }());
