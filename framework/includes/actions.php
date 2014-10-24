4b1f8b5e82984bff3e919c9fae77951c288e5cb9
Anthuan Vásquez <eigthy@gmail.com>
2014-10-24 10:09:50 -0400

    Nuevas acciones.

--- framework/includes/actions.php
+++ framework/includes/actions.php
@@ -31,3 +31,11 @@ function tm_post_before() {
 function tm_post_after() {
 	do_action( 'tm_post_after' );
 }
+
+function tm_sidebar_layout_before() {
+	do_action( 'tm_sidebar_layout_before' );
+}
+
+function tm_sidebar_layout_after() {
+	do_action( 'tm_sidebar_layout_after' );
+}
