47b00944c51809e4ee4c383f8a6c2eaa49828823
Anthuan Vásquez <eigthy@gmail.com>
2014-10-24 16:38:21 -0400

    Ya no se necesitan forman parte del archivo parts.php.

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo tm_get_local( 'search_for' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo tm_get_local( 'search' ); ?>" value="" name="s" title="<?php echo tm_get_local( 'search_for' ); ?>" />
-	</label>
-	<button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
-</form>
