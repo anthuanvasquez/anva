3e677902dbeeab7b65a96b24dde9041ed1b77c59
Anthuan Vásquez <eigthy@gmail.com>
2014-10-24 10:08:13 -0400

    Añadida nuevos do_action() para el sidebar layout.

--- page.php
+++ page.php
@@ -1,7 +1,28 @@
 <?php get_header(); ?>
 
+<?php
+	$classes = '';
+	$sidebar = tm_get_page_meta('_sidebar_column');
+
+	if ( 'left' == $sidebar || 'double_left' == $sidebar ) {
+		$classes = 'content-area right';
+
+	} elseif ( 'right' == $sidebar || 'double_right' == $sidebar  ) {
+		$classes = 'content-area left';
+
+	} elseif ( 'double' == $sidebar ) {
+		$classes = 'content-area center';
+
+	} elseif ( 'fullwidth' ) {
+		$classes = 'full-width';
+	}
+?>
+
 <div class="grid-columns">
-	<div class="content-area">
+
+	<?php tm_sidebar_layout_before(); ?>
+
+	<div class="<?php echo esc_html( $classes ); ?>">
 		<div class="main" role="main">
 
 			<?php tm_post_before(); ?>
@@ -26,7 +47,7 @@
 		</div><!-- .main (end) -->
 	</div><!-- .content-area (end) -->
 	
-	<?php get_sidebar(); ?>
+	<?php tm_sidebar_layout_after(); ?>
 	
 </div><!-- .grid-columns (end) -->
 
