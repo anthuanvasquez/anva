a87576a6affc83f07e3205cebd26cc7f85de2596
Anthuan Vásquez <eigthy@gmail.com>
2014-10-24 16:40:38 -0400

    Removido espacios.

--- header.php
+++ header.php
@@ -9,9 +9,7 @@
 	?>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	
