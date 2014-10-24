8692128c70d82fdeb103282337bdb2630154fb4d
Anthuan Vásquez <eigthy@gmail.com>
2014-10-24 10:07:21 -0400

    Añadido nuevas areas de sidebar left y right.

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,5 +1,5 @@
 <div class="widget-area right" role="complementary">
 
-	<?php if ( dynamic_sidebar( 'main-sidebar' ) ) : endif; ?>
+	<?php if ( dynamic_sidebar( 'sidebar-right' ) ) : endif; ?>
 
 </div><!-- .widget-area (end) -->
