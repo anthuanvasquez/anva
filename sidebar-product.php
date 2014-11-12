354790b4a7c67a8d3d255aef0ad53c5c1c9b89d8
Anthuan Vásquez <eigthy@gmail.com>
2014-11-12 11:27:09 -0400

    No se necesitan.

--- sidebar-product.php
+++
@@ -1,5 +0,0 @@
-<div class="widget-area product-sidebar" role="complementary">
-
-	<?php if ( dynamic_sidebar( 'product-sidebar' ) ) : endif; ?>
-
-</div><!-- .widget-area (end) -->
--- sidebar-shop.php
+++
@@ -1,5 +0,0 @@
-<div class="widget-area shop-sidebar" role="complementary">
-
-	<?php if ( dynamic_sidebar( 'shop-sidebar' ) ) : endif; ?>
-
-</div><!-- .widget-area (end) -->
