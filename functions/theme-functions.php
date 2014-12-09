1296125337cb06cac8e7dcb9a731e6f31d53e73d
anthuanvasquez <eigthy@gmail.com>
2014-12-08 23:50:28 -0400

    Para usar functions desde el tema.

---
+++ functions/theme-functions.php
@@ -0,0 +1,52 @@
+<?php
+
+/*-----------------------------------------------------------------------------------*/
+/* Theme Functions
+/*-----------------------------------------------------------------------------------*/
+
+/**
+ * Add theme support features
+ */
+function anva_theme_setup() {
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form' ) );
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'anva_theme_setup' );
+
+/**
+ * Change the slider args
+ */
+// function anva_theme_featured_size( $args ) {
+// 	if ( isset( $args['homepage'] ) ) {
+// 		$args['homepage']['size'] = 'slider_big';
+// 	}
+//  if ( ! isset( $args['homepage'] ) ) {
+// 		$args['homepage']['orderby'] = 'date';
+//  }
+// 	return $args;
+// }
+// add_filter( 'anva_slideshows', 'anva_theme_featured_size' );
+
+/**
+ * Change the start year in footer.
+ */
+function anva_theme_start_year() {
+	return 2014;
+}
+add_filter( 'anva_footer_year', 'anva_theme_start_year' );
+
+/**
+ * Change footer credits.
+ */
+// function anva_theme_footer_credits() {
+// 	return __( 'Diseno por', 'anva' );
+// }
+// add_filter( 'anva_footer_credits', 'anva_theme_footer_credits' );
+
+/**
+ * Change footer author.
+ */
+// function anva_theme_footer_author() {
+// 	return  '<a href="'. esc_url( 'http://your-url.com/') .'">Your name</a>.';
+// }
+// add_filter( 'anva_footer_author', 'anva_theme_footer_author' );
