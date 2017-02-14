5866d226f0a5085c5f10f23ac35b90c73f379111
Anthuan Vásquez <eigthy@gmail.com>
2017-02-13 20:09:21 -0400

    Changes

--- .tools/gulpfile.js/tasks/main.js
+++ .tools/gulpfile.js/tasks/main.js
@@ -11,3 +11,6 @@ gulp.task('build', ['images', 'scripts', 'styles', 'theme']);
 // Dist task chain: wipe -> build -> clean -> copy -> compress images
 // NOTE: this is a resource-intensive task!
 gulp.task('dist', ['images-optimize']);
+
+// Tests - Lints
+gulp.task('run-tests', ['styles-sass-lint', 'styles-css-lint', 'scripts-lint', 'php-lint']);
